2ecdaf23895f77512a0d4d51184570c2085c74de
Mohammad Raihanul Bashar <12432136+mraihan19@users.noreply.github.com>
2021-04-25 16:01:29 +0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
   Stream = require('node-rtsp-stream')
 stream = new Stream({
   name: 'name',
-  streamUrl: 'rtsp://admin:Experts@2021!@@24.186.96.191:554/ch01/0',
+  streamUrl: 'rtsp://username:password@link',
   wsPort: 9999,
   ffmpegOptions: { // options ffmpeg flags
     '-stats': '', // an option with no neccessary value uses a blank string
